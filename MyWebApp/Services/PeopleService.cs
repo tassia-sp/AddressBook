32c3d9ef079b472927913a7a0f1b18926357e76b
Tassia <tassiapaschoal@gmail.com>
2017-06-12 21:58:24 -0700

    Changes to layout and edits to data service

--- MyWebApp/Services/PeopleService.cs
+++ MyWebApp/Services/PeopleService.cs
@@ -1,8 +1,5 @@
 ﻿using MyWebApp.Models.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.SqlClient;
 
 namespace MyWebApp.Services
@@ -13,10 +10,11 @@ namespace MyWebApp.Services
         {
             List<Person> peopleList = new List<Person>();
 
+            //grab connction string from the Web.Config
             string connString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
             using (SqlConnection sqlConn = new SqlConnection(connString))
             {
-                ////using open and close connection without calling IDispose
+                //using opens and closes connection without calling IDispose. Prevents memory leaks
                 using(SqlCommand cmd = new SqlCommand("People_SelectAll", sqlConn))
                 {
                     sqlConn.Open();
@@ -24,15 +22,22 @@ namespace MyWebApp.Services
                     //returns a true or false
                     while (reader.Read())
                     {
+                        //instantiate the Person Model
                         Person p = new Person();
+                        //grab data from the server and place into properties of the model. 
+                        //Loops through table rows
                         p.Id = reader.GetInt32(0);
                         p.FirstName = reader.GetString(1);
                         p.LastName = reader.GetString(2);
                         p.Email = reader.GetString(3);
                         p.Favorite = reader.GetBoolean(4);
+
+                        //Add to the new Person p object
+                        peopleList.Add(p);
                     }
                 }
             }
+            //return to Api Controller
             return peopleList;
         }
     }
